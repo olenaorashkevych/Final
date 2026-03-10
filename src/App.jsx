18181d22b3c879750b4e97e9dd60c4d005b72bfe
Olena <126962979+olenaorashkevych@users.noreply.github.com>
2026-03-10 16:07:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route, Link } from 'react-router-dom'
-import Home from './pages1/Home'
+import Home from './pages/Home'
 
-import Layout from './pages1/Layout'
-import ErrorPage from './pages1/ErrorPage'
-import Lesstalk from './pages1/Lesstalk'
-import Contact from './pages1/Contact'
-import HappyCustomer from './pages1/HappyCustomer'
-import ServicesCategory from './pages1/ServicesCategory'
+import Layout from './pages/Layout'
+import ErrorPage from './pages/ErrorPage'
+import Lesstalk from './pages/Lesstalk'
+import Contact from './pages/Contact'
+import HappyCustomer from './pages/HappyCustomer'
+import ServicesCategory from './pages/ServicesCategory'
 import UiContext from './context/UiContext'
 
 export default function App() {
