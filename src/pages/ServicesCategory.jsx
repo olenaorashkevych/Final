785b793df0a3fedef6cb88266df1ad032194161a
Olena <olenaorashkevych@gmail.com>
2026-04-16 14:56:16 +0200

    Update ServicesCategory.jsx

--- src/pages/ServicesCategory.jsx
+++ src/pages/ServicesCategory.jsx
@@ -15,9 +15,7 @@ export default function ServicesSection() {
           <div className="services__card">
             <div className="services__card-content">
               <div className="services__icon">
-                <svg className="services__icon-1">
-                  <use href="public/images/sprite.svg#service1" />
-                </svg>
+                <SvgIcon id="service1" className="services__icon-1" />
               </div>
               <p className="services__name">Grooming</p>
             </div>
@@ -26,9 +24,7 @@ export default function ServicesSection() {
           <div className="services__card">
             <div className="services__card-content">
               <div className="services__icon">
-                <svg className="services__icon-2">
-                  <use href="public/images/sprite.svg#service2" />
-                </svg>
+                <SvgIcon id="service2" className="services__icon-2" />
               </div>
               <Link to="/nutrition" className="services__name">Nutrition</Link>
             </div>
