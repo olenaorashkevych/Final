bac8e1f3ddf9e4be74e7e08ccdce1b3f43d99517
Olena <olenaorashkevych@gmail.com>
2026-04-16 14:51:13 +0200

    Update SvgIcon.jsx

--- src/components/ui/SvgIcon.jsx
+++ src/components/ui/SvgIcon.jsx
@@ -1,9 +1,7 @@
-const baseUrl = import.meta.env.VITE_BASE_URL;
-
 export default function SvgIcon({ id, className = "" }) {
   return (
     <svg className={className}>
-      <use href={`${baseUrl}/images/sprite.svg#${id}`} />
+      <use href={`/images/sprite.svg#${id}`} />
     </svg>
   );
 }
